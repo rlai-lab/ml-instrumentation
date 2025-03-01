2129d1b7545930a3d40b7ffd0f66f862a8852330
andnp <andnpatterson@gmail.com>
2025-03-01 15:29:32 -0700

    feat: add ability to attach metadata to a run id

--- ml_instrumentation/_utils/sqlite.py
+++ ml_instrumentation/_utils/sqlite.py
@@ -1,6 +1,52 @@
+from collections.abc import Iterable
+from typing import Any
 from sqlite3 import Cursor
+
 
 def get_tables(cur: Cursor) -> set[str]:
     cur.row_factory = None
     res = cur.execute("SELECT name FROM sqlite_master")
     return set(r[0] for r in res.fetchall())
+
+
+def make_table(cur: Cursor, name: str, columns: Iterable[str]):
+    cols = ', '.join(map(quote, columns))
+    cur.execute(f'CREATE TABLE {name}({cols})')
+
+
+def maybe_make_table(cur: Cursor, name: str, columns: Iterable[str]):
+    tables = get_tables(cur)
+
+    if name not in tables:
+        make_table(cur, name, columns)
+
+
+def get_cols(cur: Cursor, name: str):
+    res = cur.execute(f'PRAGMA table_info({name})')
+    rows = res.fetchall()
+
+    return set(r[1] for r in rows)
+
+
+def add_cols(cur: Cursor, columns: Iterable[str]):
+    columns = map(quote, columns)
+    for col in columns:
+        cur.execute(f'ALTER TABLE results ADD COLUMN {col}')
+
+
+def ensure_table_compatible(cur: Cursor, name: str, columns: Iterable[str]):
+    columns = set(columns)
+    current_cols = get_cols(cur, name)
+    needed_cols = columns - current_cols
+
+    if needed_cols:
+        add_cols(cur, needed_cols)
+
+
+def maybe_quote(v: Any):
+    if isinstance(v, str):
+        return quote(v)
+    return v
+
+def quote(s: str):
+    return f'"{s}"'
---
+++ ml_instrumentation/metadata.py
@@ -0,0 +1,29 @@
+import sqlite3
+from pathlib import Path
+from typing import Any
+
+from filelock import FileLock
+
+import ml_instrumentation._utils.sqlite as sqlu
+
+def attach_metadata(db_path: str | Path, id: int | str, metadata: dict[str, Any]):
+    db_path = Path(db_path)
+    db_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with FileLock(f'{db_path}.lock'):
+        con = sqlite3.connect(db_path)
+        cur = con.cursor()
+
+        metadata |= {'id': id}
+        cols = metadata.keys()
+        sqlu.maybe_make_table(cur, '_metadata_', cols)
+        sqlu.ensure_table_compatible(cur, '_metadata_', cols)
+
+        inserter = ', '.join(['?'] * len(cols))
+
+        header = ', '.join(map(sqlu.quote, cols))
+        query = f'INSERT INTO _metadata_ ({header}) VALUES ({inserter})'
+        print(query)
+        cur.execute(query, list(metadata.values()))
+        con.commit()
+        con.close()
